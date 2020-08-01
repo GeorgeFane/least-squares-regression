5170d6c02c41680b05945c300046620138b413a4
GeorgeFane <39439818+GeorgeFane@users.noreply.github.com>
2020-08-01 18:49:21 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import dash
+from dash import Dash
 import dash_core_components as dcc
 import dash_html_components as html
 
@@ -6,10 +6,6 @@ from dash.dependencies import Input, Output
 from dash_table import DataTable
 
 from random import uniform
-
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-server = app.server
 
 def getPairs(string):
     row=list(map(float, string.split()))
@@ -44,13 +40,6 @@ def clear(u, v):
     constant=v[i]
     return [y-constant*x for x, y in zip(u, v)]
 
-def randX(down, up):
-    return sorted([uniform(down, up) for i in range(99)])
-
-def getY(x, degrees):
-    a=getA(x, degrees)
-    return [dot(a0, coefs) for a0 in a]
-
 def reduce(matrix):
     height=len(matrix)
     
@@ -79,34 +68,76 @@ def getCoefs(x, y, degrees):
     
     return [row[-1] for row in reduce(system)]
 
+def randX(down, up):
+    return sorted([uniform(down, up) for i in range(99)])
+
+f = lambda coefs, x: sum([coef*x**power for power, coef in enumerate(coefs)])
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = Dash(__name__, external_stylesheets=external_stylesheets)
+
 app.layout = html.Div([
     html.H1('Least-Squares Regression'),
     
     html.Label('Space-Separated Data: '),
-    dcc.Input(id='points'),
+    dcc.Input(id='points', value='1 0 2 3 3 7 4 14 5 22'),
     html.Br(),
+
     html.Label('For example, input points (1, 0), (2, 3), (3, 7), (4, 14), (5, 22) as 1 0 2 3 3 7 4 14 5 22'),
     html.Br(),
-
-    html.Label('Degrees of Best-Fit Line: '),
-    dcc.Input(id='degrees', type='number', value=-1),    
-    html.Div(id='eq'),
     html.Br(),
 
-    html.Label('Model Prediction for x = '),
-    dcc.Input(id='x', type='number'),
-    html.Br(),
-    html.Div(id='y'),
-    
-    dcc.Graph(id='graph'),    
+    html.Div([
+        html.Div([
+            html.Label('Degrees of Best-Fit Line: '),
+            dcc.Input(id='degrees', type='number', value=2),
+            
+            html.Div(id='eq'),
+            
+            html.Div(id='coefs', style={'display': 'none'}),
+            
+            dcc.Graph(id='graph'), 
+            html.Br(),   
+
+            html.Label('Model Prediction for x = '),
+            dcc.Input(id='x', type='number'),
+            html.Br(),
+            html.Div(id='y'),
+        ], className="six columns"),
+
+        html.Div([
+            html.Label('n-th Derivative of Model'),
+            dcc.Dropdown(id='derivDrop'),
+            html.Br(),
+            
+            html.Div(
+                id='derivs', 
+                style={'display': 'none'},
+            ),
+            
+            html.Div(id='derivEq'),
+            
+            dcc.Graph(id='derivGraph'), 
+            html.Br(),   
+
+            html.Label('n-th Derivative of Model at x = '),
+            dcc.Input(id='xDeriv', type='number'),
+            html.Br(),
+            html.Div(id='yDeriv'),
+        ], className="six columns"),
+    ], className="row"),
 ])
 
-coefs=1
+getEq = lambda coefs: ' + '.join([f'({round(coef, 9)}) x^{i}' for i, coef in enumerate(coefs)])
+
+import json
 
 @app.callback(
     [
         Output('graph', 'figure'),
         Output('eq', 'children'),
+        Output('coefs', 'children'),
     ],
     [
         Input('points', 'value'),
@@ -116,6 +147,7 @@ coefs=1
 def fillChart(points, degrees):
     x, y=transpose(getPairs(points))
     eq=''
+    coefs=[0]
           
     traces=[
         dict(
@@ -127,11 +159,10 @@ def fillChart(points, degrees):
     ]
     
     if degrees>=0:
-        global coefs
         coefs=getCoefs(x, y, degrees)
 
         x1=randX(min(x), max(x))
-        y1=getY(x1, degrees)
+        y1=[f(coefs, x) for x in x1]
 
         traces.append(
             dict(
@@ -141,7 +172,7 @@ def fillChart(points, degrees):
             )
         )
         
-        eq=f'Model: f (x, {degrees}) = ' + ' + '.join([f'({round(coef, 9)}) x^{i}' for i, coef in enumerate(coefs)])
+        eq=f'Model: f (x) = ' + getEq(coefs)
     
     return (
         dict(
@@ -151,17 +182,90 @@ def fillChart(points, degrees):
             )
         ),
         eq,
+        json.dumps(coefs),
     )
 
 @app.callback(
     Output('y', 'children'),
     [
         Input('x', 'value'),
-        Input('degrees', 'value'),
+        Input('coefs', 'children'),
     ]
 )
-def predict(x, degrees):       
-    return f'f ({x}, {degrees}) = {round(getY([x], degrees)[0], 9)}'
+def predict(x, coefsString):       
+    coefs=json.loads(coefsString)
+    return f'f ({x}) = {round(f(coefs, x), 9)}'
+
+derive = lambda coefs: [coef*(power+1) for power, coef in enumerate(coefs[1:])]
+
+@app.callback(
+    [
+        Output('derivs', 'children'),    
+        Output('derivDrop', 'options'), 
+    ],
+    [
+        Input('coefs', 'children'),
+    ]
+)
+def fillDrop(coefsString):       
+    coefs=json.loads(coefsString)
+    derivs=[coefs]
+    while len(derivs[-1])>1:
+        derivs.append(derive(derivs[-1]))
+
+    return [
+        json.dumps(derivs),
+        [dict(label = i, value = i) for i in range(len(derivs))],
+    ]
+
+@app.callback(
+    [   
+        Output('derivGraph', 'figure'), 
+        Output('derivEq', 'children'), 
+    ],
+    [
+        Input('points', 'value'),
+        Input('derivs', 'children'),    
+        Input('derivDrop', 'value'), 
+    ],
+)
+def plotDeriv(points, derivsString, nDeriv): 
+    x, y=transpose(getPairs(points))
+    derivs=json.loads(derivsString)
+    coefs=derivs[nDeriv]
+
+    x1=randX(min(x), max(x))
+    y1=[f(coefs, x) for x in x1]
+    
+    return (
+        dict(
+            data=[
+                dict(
+                    x=x1,
+                    y=y1,
+                    name='Line of Best Fit'
+                ),
+            ],
+            layout=dict(
+                hovermode='closest'
+            )
+        ),
+        f'f_{nDeriv} (x) = {getEq(coefs)}',
+    )
+
+@app.callback(
+    Output('yDeriv', 'children'),
+    [
+        Input('xDeriv', 'value'),
+        Input('derivs', 'children'),  
+        Input('derivDrop', 'value'), 
+    ]
+)
+def predictDeriv(x, derivsString, nDeriv):    
+    derivs=json.loads(derivsString)
+    coefs=derivs[nDeriv]
+
+    return f'f_{nDeriv} ({x}) = {round(f(coefs, x), 9)}'
 
 if __name__ == '__main__':
     app.run_server()
